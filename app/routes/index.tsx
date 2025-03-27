1517a05ef3adcdc3fba875f0c7ad8dc83e4c00da
Matsuoka Kensuke <quantumular@gmail.com>
2025-03-27 19:21:43 +0900

    tanstack start

---
+++ app/routes/index.tsx
@@ -0,0 +1,12 @@
+import { createFileRoute, useRouter } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/")({
+  component: Home,
+});
+
+function Home() {
+  const router = useRouter();
+  const state = Route.useLoaderData();
+
+  return <div>Hello world</div>;
+}
