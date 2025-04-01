6f7d7a6b9df1be9db8f135a9646d3c1a2d9574f5
Matsuoka Kensuke <quantumular@gmail.com>
2025-04-01 14:10:02 +0900

    mock chat

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,5 +1,8 @@
+import { openai } from "@ai-sdk/openai";
+import { streamText } from "ai";
 import { Hono } from "hono";
 import { cors } from "hono/cors";
+import { stream } from "hono/streaming";
 
 import { auth } from "./lib/auth";
 
@@ -22,6 +25,25 @@ app.on(["POST", "GET"], "/api/auth/*", (c) => {
 });
 app.get("/", (c) => {
 	return c.text("Hello from Hono🔥");
+});
+
+app.use("/chat", cors({ origin: "http://localhost:3000" }));
+app.post("/chat", async (c) => {
+	const { messages } = await c.req.json();
+	const result = streamText({
+		model: openai("gpt-4o"),
+		messages,
+	});
+
+	// Need this for Cloudflare Workers
+	// See also: https://hono.dev/docs/helpers/streaming#streamtext
+	c.header("Content-Encoding", "Identity");
+
+	// Mark the response as a Vercel AI SDK v1 data stream:
+	c.header("X-Vercel-AI-Data-Stream", "v1");
+	c.header("Content-Type", "text/plain; charset=utf-8");
+
+	return stream(c, (stream) => stream.pipe(result.toDataStream()));
 });
 
 export default app;
--- apps/web/app/routes/index.tsx
+++ apps/web/app/routes/index.tsx
@@ -1,3 +1,4 @@
+import { useChat } from "@ai-sdk/react";
 import { createFileRoute, redirect } from "@tanstack/react-router";
 import { createServerFn } from "@tanstack/react-start";
 
@@ -19,6 +20,9 @@ export const Route = createFileRoute("/")({
 function Home() {
 	const text = Route.useLoaderData();
 	const { data, isPending } = authClient.useSession();
+	const { messages, input, handleInputChange, handleSubmit } = useChat({
+		api: "http://localhost:8787/chat",
+	});
 
 	if (isPending) {
 		return <div>Loading...</div>;
@@ -29,10 +33,37 @@ function Home() {
 			<p>Hono says: {text}</p>
 			{data ? (
 				<div>
-					<pre>{JSON.stringify(data, null, 2)}</pre>
 					<button type="button" onClick={() => authClient.signOut()}>
 						Sign out
 					</button>
+					<form onSubmit={handleSubmit}>
+						<input
+							type="text"
+							value={input}
+							onChange={handleInputChange}
+							placeholder="Type a message..."
+						/>
+						<button type="submit">Send</button>
+					</form>
+					<div>
+						{messages.map((message) => (
+							<div key={message.id} className="whitespace-pre-wrap">
+								{message.role === "user" ? "User: " : "AI: "}
+								{message.parts.map((part, i) => {
+									switch (part.type) {
+										case "text":
+											return <div key={`${message.id}-${i}`}>{part.text}</div>;
+										case "tool-invocation":
+											return (
+												<pre key={`${message.id}-${i}`}>
+													{JSON.stringify(part.toolInvocation, null, 2)}
+												</pre>
+											);
+									}
+								})}
+							</div>
+						))}
+					</div>
 				</div>
 			) : (
 				<button
