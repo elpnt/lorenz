e7fec869ab23473e112cccd109b7577e6d841034
Matsuoka Kensuke <quantumular@gmail.com>
2025-04-18 23:34:50 +0900

    wip: chat

--- apps/web/src/routes/_app/chat.tsx
+++ apps/web/src/routes/_app/chat.tsx
@@ -1,5 +1,10 @@
 import { useChat } from "@ai-sdk/react";
-import { PaperAirplaneIcon, StopIcon } from "@heroicons/react/16/solid";
+import {
+	CheckCircleIcon,
+	InformationCircleIcon,
+	PaperAirplaneIcon,
+	StopIcon,
+} from "@heroicons/react/16/solid";
 import { createFileRoute } from "@tanstack/react-router";
 import { useEffect, useRef } from "react";
 
@@ -10,16 +15,30 @@ export const Route = createFileRoute("/_app/chat")({
 	component: RouteComponent,
 });
 
-const UserMessage = ({ text }: { text: string }) => (
-	<div className="flex justify-end">
-		<div className="max-w-lg bg-zinc-100 px-4 py-2.5 dark:bg-zinc-700 dark:text-white rounded-xl">
+export function Note({ children }: { children: React.ReactNode }) {
+	return (
+		<div className="my-6 flex gap-2.5 rounded-2xl border border-emerald-500/20 bg-emerald-50/50 p-4 leading-6 text-emerald-900 dark:border-emerald-500/30 dark:bg-emerald-500/5 dark:text-emerald-200 dark:[--tw-prose-links-hover:theme(colors.emerald.300)] dark:[--tw-prose-links:theme(colors.white)]">
+			<InformationCircleIcon className="mt-1 h-4 w-4 flex-none fill-emerald-500 stroke-white dark:fill-emerald-200/20 dark:stroke-emerald-200" />
+			<div className="[&>:first-child]:mt-0 [&>:last-child]:mb-0">
+				{children}
+			</div>
+		</div>
+	);
+}
+
+const UserMessage = ({ text, ok }: { text: string; ok?: boolean }) => (
+	<div className="flex justify-end items-center gap-1">
+		{ok && (
+			<CheckCircleIcon className="w-5 h-5 text-emerald-400 animate-scale-rotate-in" />
+		)}
+		<div className="max-w-64 sm:max-w-lg bg-zinc-100 px-4 py-2.5 dark:bg-zinc-700 dark:text-white rounded-xl">
 			{text}
 		</div>
 	</div>
 );
 
 function RouteComponent() {
-	const { messages, input, handleInputChange, handleSubmit, isLoading, stop } =
+	const { messages, input, handleInputChange, handleSubmit, status, stop } =
 		useChat({
 			api: "http://localhost:8787/chat",
 			maxSteps: 2,
@@ -35,43 +54,60 @@ function RouteComponent() {
 		<>
 			<div className="flex-1 overflow-y-scroll p-6 lg:p-10 ">
 				<div className="mx-auto max-w-3xl space-y-8">
-					{messages.map((message) => (
+					{messages.map((message, i) => (
 						<div key={message.id} className="whitespace-pre-wrap">
 							{message.role === "user" ? (
-								<UserMessage text={message.content} />
+								<UserMessage
+									text={message.content}
+									ok={(function getProofreadOK() {
+										const nextMsg = messages[i + 1];
+										if (
+											nextMsg &&
+											nextMsg.role !== "user" &&
+											nextMsg.parts?.some(
+												(part) =>
+													part.type === "tool-invocation" &&
+													part.toolInvocation.toolName === "proofread" &&
+													part.toolInvocation.state === "result" &&
+													part.toolInvocation.result &&
+													part.toolInvocation.result.ok === true,
+											)
+										)
+											return true;
+										return false;
+									})()}
+								/>
 							) : (
 								<div className="">
 									{message.parts.map((part, index) => {
 										switch (part.type) {
 											case "text":
-												// biome-ignore lint:
-												return <div key={index}>{part.text}</div>;
+												return (
+													// biome-ignore lint:
+													<div key={index} className="dark:text-white">
+														{part.text}
+													</div>
+												);
 											case "tool-invocation": {
 												const callId = part.toolInvocation.toolCallId;
 												switch (part.toolInvocation.state) {
 													case "call":
-														return (
-															<pre key={callId}>
-																{JSON.stringify(part.toolInvocation, null, 2)}
-															</pre>
-														);
-													case "partial-call":
-														return (
-															<pre key={callId}>
-																{JSON.stringify(part.toolInvocation, null, 2)}
-															</pre>
-														);
+														return <div key={callId}>Reviewing...</div>;
+													// case "partial-call":
+													// 	return (
+													// 		<pre key={callId}>
+													// 			{JSON.stringify(part.toolInvocation, null, 2)}
+													// 		</pre>
+													// 	);
 													case "result": {
 														const result: ToolResult["result"] =
 															part.toolInvocation.result;
+														if (result.ok) return null;
 														return (
-															<pre key={callId} className="text-xs">
-																{JSON.stringify(
-																	part.toolInvocation.result,
-																	null,
-																	2,
-																)}
-															</pre>
+															<Note key={callId}>
+																<strong>{result.corrected}</strong>
+																<p>{result.explanation}</p>
+															</Note>
 														);
 													}
 												}
@@ -100,7 +136,7 @@ function RouteComponent() {
 							className="text-base/6 sm:text-sm/6 resize-none size-full focus:outline-none"
 						/>
 						<div className="flex justify-end">
-							{isLoading ? (
+							{status === "streaming" ? (
 								<Button type="button" size="icon" onClick={stop}>
 									<StopIcon />
 								</Button>
