9b309380f5bc7dedf449e1991536e2b737decb1e
Matsuoka Kensuke <quantumular@gmail.com>
2025-04-19 15:35:56 +0900

    fix styles

--- apps/web/src/components/ui/checkbox.tsx
+++ apps/web/src/components/ui/checkbox.tsx
@@ -9,18 +9,19 @@ export function CheckboxField({ children }: { children: React.ReactNode }) {
 	return (
 		<div
 			data-slot="field"
-			className={clsx(
+			className={clsx([
 				// Base layout
-				"grid grid-cols-[1.125rem_1fr] gap-x-4 gap-y-1 sm:grid-cols-[1rem_1fr]",
-				// Control layout
-				"*:data-[slot=control]:col-start-1 *:data-[slot=control]:row-start-1 *:data-[slot=control]:mt-0.75 sm:*:data-[slot=control]:mt-1",
-				// Label layout
-				"*:data-[slot=label]:col-start-2 *:data-[slot=label]:row-start-1",
-				// Description layout
-				"*:data-[slot=description]:col-start-2 *:data-[slot=description]:row-start-2",
-				// With description
+				// "grid grid-cols-[1.125rem_1fr] gap-x-4 gap-y-1 sm:grid-cols-[1rem_1fr]",
+				// // Control layout
+				// "*:data-[slot=control]:col-start-1 *:data-[slot=control]:row-start-1 *:data-[slot=control]:mt-0.75 sm:*:data-[slot=control]:mt-1",
+				// // Label layout
+				// "*:data-[slot=label]:col-start-2 *:data-[slot=label]:row-start-1",
+				// // Description layout
+				// "*:data-[slot=description]:col-start-2 *:data-[slot=description]:row-start-2",
+				"*:data-[slot=description]:pl-[1.625rem] sm:*:data-[slot=description]:pl-6",
+				// // With description
 				"has-data-[slot=description]:**:data-[slot=label]:font-medium",
-			)}
+			])}
 		>
 			{children}
 		</div>
